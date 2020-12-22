82fb1e52b373f4e0ea19a07a14693b9f7178cbd6
DanielHss <dh99@live.se>
2020-12-22 14:43:39 +0100

    Fetched default code from school into project
    I also wanted to add typescript into the code but decided in the last minute against it because it will then be more stuff to learn in a short span of time. Maybe in the front-end I will try to import typescript together with vue class. That sounds quite neat.

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,13 @@
-console.log("Hello world from backend")
+const express = require("express");
+const session = require("express-session");
+const store = require("better-express-store");
+
+const app = express();
+const RestApi = require("./RestApi");
+const ACL = require("./middleware/ACL");
+const ACLsettings = require("./middleware/ACLsettings");
+
+//Allow JSON in Express
+app.use(express.json());
+
+//Prevent badly formated JSON in req.body
---
+++ backend/logic/Encrypt.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+//const { performance } = require('perf_hooks');
+
+module.exports = class Encrypt {
+  // Sha 256-encryption with
+  // the build in Node.js module crypto
+  static encrypt(password) {
+    return crypto
+      .createHmac("sha256", require("./salt.json"))
+      .update(password)
+      .digest("hex");
+  }
+  static multiEncrypt(password, encryptTimes = 9999) {
+    while (encryptTimes--) {
+      password = this.encrypt(password);
+    }
+    return password;
+  }
+};

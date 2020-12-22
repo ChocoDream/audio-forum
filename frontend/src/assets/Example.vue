97cb608903f35349964ac57267f747d98e61b90a
DanielHss <dh99@live.se>
2020-12-22 17:07:08 +0100

    setting up

--- frontend/src/assets/Example.vue
+++ frontend/src/assets/Example.vue
@@ -2,20 +2,20 @@
   <div>
     <h1>THIS IS JUST AN EXAMPLE</h1>
     <p>{{ message }}</p>
-    <button @click="addCount">Add count {{count}} </button>
+    <button @click="addCount">Add count {{ count }}</button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 
 @Component({})
 export default class Example extends Vue {
-  @Prop({type: String}) message;
+  @Prop({ type: String }) message;
 
-  count: number = 0;
-  addCount() {
+  count = 0;
+  addCount(): void {
     this.count++;
   }
 }

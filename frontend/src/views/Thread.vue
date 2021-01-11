c5027de60eb566e81aa1806b41eb1c2589d25d8c
DanielHss <dh99@live.se>
2021-01-11 23:28:44 +0100

    Fetching data for Posts from datanbase using ThreadId from param

--- frontend/src/views/Thread.vue
+++ frontend/src/views/Thread.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
     <p>Hello from Thread</p>
-    <p> {{$route.params}} </p>
+    <p>{{ $route.params }}</p>
+    <ul class="list-group">
+      <Post v-for="(item, i) of postData" :key="`${item.id}+${i}`" :post="item" />
+    </ul>
   </div>
 </template>
 
-<script>
-import Vue from 'vue'
-import {Component} from 'vue-property-decorator'
-@Component
-export default class Thread extends Vue{
-  
+<script lang="ts">
+import Post from "../components/Thread/Post.vue";
+
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component({
+  components: {
+    Post,
+  },
+})
+export default class Thread extends Vue {
+  $store: any
+  $route: any
+
+  get postData() {
+    return this.$store.state.posts || []
+  }
+
+  created() {
+    this.$store.dispatch("fetchPostsFromId", this.$route.params.thread);
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.list-group-item {
+  list-style: none;
+}
 </style>

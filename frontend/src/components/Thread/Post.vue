c4bfee27d59f4f2aa2b93f939a95df9592c231de
DanielHss <dh99@live.se>
2021-01-11 23:29:03 +0100

    Forgot to push the post item in the previous push

--- frontend/src/components/Thread/Post.vue
+++ frontend/src/components/Thread/Post.vue
@@ -1,16 +1,20 @@
 <template>
-  
+  <li class="post-info list-group-item">
+    <p>{{post.content}}</p>
+  </li>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator'
+import {Component, Prop} from 'vue-property-decorator'
 @Component
 export default class Post extends Vue {
-
+  @Prop({type: Object}) post: any
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.post-info{
+  list-style: none;
+}
 </style>

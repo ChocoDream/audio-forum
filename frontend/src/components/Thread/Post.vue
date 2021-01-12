26548183d4411b82272f76c2f093e0902b787986
DanielHss <dh99@live.se>
2021-01-12 20:47:51 +0100

    Messy way to fetch user with username

--- frontend/src/components/Thread/Post.vue
+++ frontend/src/components/Thread/Post.vue
@@ -1,20 +1,43 @@
 <template>
   <li class="post-info list-group-item">
-    <p>{{post.content}}</p>
+    <div class="row">
+      <div class="col align-self-start col-3">
+        <div class="row">
+          <div class="col-align-self-start col-2"><i>M</i></div>
+          <div class="col align-self-end col-10">{{ post.timestamp }}</div>
+        </div>
+      </div>
+      <div class="col align-self-end col-9"></div>
+    </div>
+    <div class="row">
+      <div class="col align-self-start col-3">{{ user.username }}</div>
+      <div class="col col-9">
+        <p>{{ post.content }}</p>
+      </div>
+    </div>
   </li>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator'
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
 @Component
 export default class Post extends Vue {
-  @Prop({type: Object}) post: any
+  @Prop({ type: Object }) post: any;
+
+  user = { username: "DEFAULT" };
+  async mounted() {
+    await fetch(`/api/users/${this.post.userId}`)
+      .then((response) => {
+        if (response.ok) return response.json();
+      })
+      .then((data) => (this.user = data));
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.post-info{
+.post-info {
   list-style: none;
 }
 </style>

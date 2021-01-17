837984baaaf93c701df17f3b9be5c6aea1247a2b
DanielHss <dh99@live.se>
2021-01-17 17:07:06 +0100

    Added routing guard protection, prevent guests to access some routes
    As users to access admin route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import store from "../store/index";
 
 Vue.use(VueRouter);
 
@@ -38,11 +39,32 @@ const routes: Array<RouteConfig> = [
     path: "/profile",
     name: "Profile",
     component: () => import("../views/Profile.vue"),
+    meta: { requiresAuth: true },
   },
 ];
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (to.matched.some((record) => record.meta.requiresAdmin)) {
+      if (store.state.currentUser.roles.includes("adminstrator")) {
+        next();
+        return;
+      } else {
+        //Send user to forbidden page
+        next("/");
+      }
+    } else if (store.state.currentUser.roles.includes("user")) {
+      next();
+      return;
+    }
+    next("/log-in");
+  } else {
+    next();
+  }
 });
 
 export default router;

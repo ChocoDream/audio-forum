13a5c1523b87c28c5e3b86ecab0c954262e7f285
DanielHss <dh99@live.se>
2021-01-17 17:07:31 +0100

    Made a simple overview of your account in Profile.

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <p>My Profile</p>
+    <h2>ID: {{ user.id }}</h2>
+    <h2>Username: {{ user.username }}</h2>
+    <h2>Email: {{ user.email }}</h2>
+    <h2>Roles: {{ roles }}</h2>
+    <h2 v-if="user.roles.includes('moderator')">
+      Moderator over subforums: {{ subforumNames }}
+    </h2>
   </div>
 </template>
 
@@ -8,7 +14,31 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 @Component
-export default class Profile extends Vue {}
+export default class Profile extends Vue {
+  $store: any;
+  get user() {
+    return this.$store.state.currentUser;
+  }
+
+  get roles() {
+    return this.user.roles.join(", ");
+  }
+
+  get subforumNames() {
+    if (
+      !(
+        this.user.roles.includes("moderator") ||
+        this.user.roles.includes("adminstrator")
+      )
+    )
+      return "";
+    if (this.user.roles.includes("adminstrator")) {
+      return "Every subforum";
+    } else {
+      return this.user.moderatorSubForumId.join(", ");
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss"></style>

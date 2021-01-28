dec658d9c0f5ce025315e729ca6abf9fd52679a9
DanielHss <dh99@live.se>
2021-01-28 15:16:24 +0100

    duplicate roles fix

--- backend/logic/utils.js
+++ backend/logic/utils.js
@@ -1,26 +1,32 @@
 module.exports.getUsersFromArray = getUsersFromArray = (array) => {
   //thx stackoverflow
   const seen = new Map();
-  return array.filter((entry) => {
-    let previous;
-    if (seen.has(entry.id)) {
-      previous = seen.get(entry.id);
-      previous.roles.push(entry.roles);
-      if (entry.subforumId) {
-        previous.subforumId.push(entry.subforumId);
+  return array
+    .filter((entry) => {
+      let previous;
+      if (seen.has(entry.id)) {
+        previous = seen.get(entry.id);
+        previous.roles.push(entry.roles);
+        if (entry.subforumId) {
+          previous.subforumId.push(entry.subforumId);
+        }
+
+        return false;
       }
 
-      return false;
-    }
+      if (!Array.isArray(entry.roles)) {
+        entry.roles = [entry.roles];
+        entry.subforumId = [];
+      }
 
-    if (!Array.isArray(entry.roles)) {
-      entry.roles = [entry.roles];
-      entry.subforumId = [];
-    }
-
-    seen.set(entry.id, entry);
-    return true;
-  });
+      seen.set(entry.id, entry);
+      return true;
+    })
+    .map((user) => {
+      const roles = new Set(user.roles);
+      user.roles = [...roles];
+      return user;
+    });
 };
 
 module.exports.isModeratorOrAbove = isModeratorOrAbove = (user, req) => {
